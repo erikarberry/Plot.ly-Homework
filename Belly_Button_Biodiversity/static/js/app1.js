682b0e8b1c7ab3bbd82ddf0f01361e51d7109d62
Erika Berry <erikarberry@gmail.com>
2020-03-14 11:39:35 -0400

    initial upload

---
+++ Belly_Button_Biodiversity/static/js/app1.js
@@ -0,0 +1,105 @@
+function getData(sample) {
+    // read the json file to get data
+    d3.json("data/samples.json").then((data)=> {
+        var metadata = data.metadata;
+        console.log(metadata)
+
+    var data = metadata.filter(meta => meta.id.toString() === id)[0];
+    var sampleData = d3.select("#sample-metadata");
+    sampleData.html("");
+    // grab the necessary data per id and append the info into the panel
+    Object.entries(data).forEach(function([key, value]) {   
+        var row = sampleData.append("p");
+        row.text(`{key}:${value}`);
+        // var row = sampleData.append("tr");
+        // var cell = row.append("td"); 
+        // cell.text(`{key}:${value}`);
+        });
+    });
+}
+
+// Create a horizontal bar chart with a dropdown menu to display the top 10 OTUs found in that individual.
+function getPlot(sample) {
+    // read the json file to get data
+    d3.json("data/samples.json").then((data)=> {
+        var metadata = data.metadata;
+        console.log(metadata)
+        // filter by id 
+        var toptensamples = data.sample_values.slice(0,10);
+        console.log(toptensamples);
+        var otu_id = data.otu_ids.slice(0,10);
+        console.log(otu_id);
+        var otu_labels = data.otu_labels.slice(0,10);
+        console.log(otu_labels);
+  
+        var barChart = {
+            x: samplevalues,
+            y: otu_id,
+            text: otu_labels,
+            // marker: {
+            //   color: 'rgb(142,124,195)'},
+            type:"bar",
+            orientation: "h",
+        };
+  
+        var data1 = [barChart];
+  
+        var layout1 = {
+            title: "Top 10 OTU",
+            yaxis:{
+                tickmode:"linear",
+            },
+            margin: {
+                l: 100,
+                r: 100,
+                t: 100,
+                b: 30
+            }
+        };
+        // Create a bubble chart that displays each sample
+        var BubbleChart = {
+            x: otu_id,
+            y: toptensamples,
+            mode: "markers",
+            text: otu_labels,
+            marker: {
+                size: toptensamples,
+                color: otu_ids
+            }
+        };
+        var data2 = [BubbleChart];
+        var layout2 = {
+            height: 600,
+            width: 1000,
+            hovermode: "closets",
+            xaxis:{title: "OTU ID"},
+        };
+    // Draw plot
+    Plotly.newPlot("bar", data1, layout1);
+    // Draw plots
+    Plotly.newPlot("bubble", data2, layout2); 
+    });
+};  
+
+// create the function for the initial data rendering
+function init() {
+    // select dropdown menu 
+    var selector = d3.select("#selDataset");
+    // read the json file to get data
+    d3.json("data/samples.json").then((sampleNames)=> {
+        sampleNames.forEach((data) => {
+            selector.append("option").text(data).property("value",data);
+        });
+        const first = sampleNames[0];
+        getData(first);
+        getPlot(first);
+    });
+}
+// create the function for the change event
+function optionChanged(nextSample) {
+    getData(nextSample);
+    getPlot(nextSample);
+}
+
+// Initialize the dashboard
+init();
